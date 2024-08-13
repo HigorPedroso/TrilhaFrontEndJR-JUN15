8605c95fae6e7b365a4f35940b0a84c1da7003c7
HigorPedroso <higor533@gmail.com>
2024-08-12 22:17:24 -0300

    Feat: add header with menu mobile and desktop

---
+++ trilhaFrondEnd/src/components/header/Header.tsx
@@ -0,0 +1,59 @@
+import { useState } from "react";
+import "./Header.css";
+import { IconContext } from "react-icons";
+
+import { FaWindowClose } from "react-icons/fa";
+import { GiHamburgerMenu } from "react-icons/gi";
+
+const Header = () => {
+  const [menuMobile, setMenuMobile] = useState<Boolean>(false);
+  const menuOptions = [
+    ["Habilidades", "#skills"],
+    ["Hobbies", "#hobbies"],
+    ["Motivação", "#motivation"],
+  ];
+
+  const handleMenuOpen = () => {
+    setMenuMobile(!menuMobile);
+  };
+
+  return (
+    <div className="header">
+      <div className="header__container">
+      <div className="header__options">
+        <div className="header__logo">
+          <img src="./img/logo.png" alt="Logo" className="header__logo-img" />
+        </div>
+        <div className="header__menu menu">
+          <IconContext.Provider
+            value={{ className: "menu__icon", size: "30px" }}
+          >
+            <div
+              className={`${menuMobile ? "rotate" : "rotate-inverse"}`}
+              onClick={handleMenuOpen}
+            >
+              {menuMobile ? <FaWindowClose /> : <GiHamburgerMenu />}
+            </div>
+          </IconContext.Provider>
+        </div>
+      </div>
+
+      <nav className={`menu__nav ${menuMobile ? "visible" : "hidden"} `}>
+        <ul className="menu__list">
+          {menuOptions.map((option) => (
+            <li className="menu__item" onClick={handleMenuOpen}>
+              <a className="menu__item-link" href={option[1]}>
+                {option[0]}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </nav>
+      </div>
+      
+      
+    </div>
+  );
+};
+
+export default Header;

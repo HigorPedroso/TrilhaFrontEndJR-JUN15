056a12f608e7663c770734ab7621fde81c79e53e
HigorPedroso <higor533@gmail.com>
2024-08-13 16:11:51 -0300

    Feat: add loading page

--- trilhaFrondEnd/src/App.tsx
+++ trilhaFrondEnd/src/App.tsx
@@ -1,23 +1,38 @@
+import { useEffect, useState } from 'react'
 import './App.css'
 import FeaturedHeader from './components/featured-header/FeaturedHeader'
 import Footer from './components/footer/Footer'
 import Header from './components/header/Header'
 import Hobbies from './components/hobbies/Hobbies'
+import Loader from './components/loader/Loader'
 import Motivation from './components/motivation/Motivation'
 import Skills from './components/skills/Skills'
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 3000);
+  }, []);
 
   return (
     <>
-      <Header />
-      <FeaturedHeader />
-      <Skills />
-      <Hobbies />
-      <Motivation />
-      <Footer />
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <>
+          <Header />
+          <FeaturedHeader />
+          <Skills />
+          <Hobbies />
+          <Motivation />
+          <Footer />
+        </>
+      )}
     </>
-  )
+  );
 }
 
 export default App
---
+++ trilhaFrondEnd/src/components/loader/Loader.tsx
@@ -0,0 +1,13 @@
+import "./Loader.css";
+
+const Loader = () => {
+  return (
+    <div className="loader">
+      <div className="loading-container">
+        <div className="loading"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Loader;
